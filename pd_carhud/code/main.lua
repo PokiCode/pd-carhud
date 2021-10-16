d5841757698b6b8372658643e93122525132b914
PokiDev <72568407+PokiCode@users.noreply.github.com>
2021-10-17 00:01:45 +0200

    Add files via upload

---
+++ pd_carhud/code/main.lua
@@ -0,0 +1,45 @@
+ESX = nil
+TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
+
+local io = false
+
+Citizen.CreateThread(function()
+    while true do
+        local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
+        local ped = GetPlayerPed(-1)
+        local vehicleSpeed = GetEntitySpeed(vehicle)
+        local vehicleGear = GetVehicleCurrentGear(vehicle)
+        local vehicleFuel = GetVehicleFuelLevel(vehicle)
+        local vehicleHealth = GetVehicleEngineHealth(vehicle)
+        local m = 1000
+
+        if IsPedInAnyVehicle(ped, false) and GetIsVehicleEngineRunning(vehicle) and GetPedInVehicleSeat(vehicle, ped) then
+            if Config["UseKM"] == true then
+                SendNUIMessage({
+                    action = "show";
+                    showSpeed = vehicleSpeed * 3.18;
+                    showGear = "GEAR " .. vehicleGear;
+                    showFuel = ESX.Math.Round(vehicleFuel),
+                    showVehicleState = ESX.Math.Round(vehicleHealth / 10),
+                })
+                m = 100
+            else
+                SendNUIMessage({
+                    action = "show";
+                    showSpeed = vehicleSpeed;
+                    showGear = "GEAR " .. vehicleGear;
+                    showFuel = ESX.Math.Round(vehicleFuel),
+                    showVehicleState = ESX.Math.Round(vehicleHealth / 10),
+                })
+                m = 100
+            end
+        else
+            SendNUIMessage({
+                action = "hide";
+            })
+            m = 1000
+        end 
+
+        Citizen.Wait(m)
+    end
+end)
---
+++ pd_carhud/fxmanifest.lua
@@ -0,0 +1,22 @@
+-- FX Info
+fx_version 'adamant'
+game 'gta5'
+
+-- Credits
+author 'PokiDev'
+description 'Simple Carhud for FiveM ESX Framework'
+
+-- Resources
+ui_page 'html/index.html'
+
+files {
+    "html/index.html",
+    "html/styles.css",
+    "html/digital.ttf",
+    "html/main.js"
+}
+
+client_scripts {
+    "config.lua",
+    "code/main.lua"
+}
